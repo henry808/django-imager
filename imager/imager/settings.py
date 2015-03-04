58def11d19f36317e547db47a4fd7fda93174c71
Henry Grantham <hgrantham@gmail.com>
2015-03-03 17:00:17 -0800

    changes to models.py and added postgres to settings.py

--- imager/imager/settings.py
+++ imager/imager/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'imager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'imager',
+        'USER': 'JustinKan',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -69,7 +73,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Pacific'
 
 USE_I18N = True
 
--- imager/imagerapp/models.py
+++ imager/imagerapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# from django.db.models.signals import post_save, pre_delete
+
 # Already in User:
 # username,
 # password,
@@ -16,21 +18,32 @@ class ImagerProfile(models.Model):
         ('PU', 'Public'),
     )
 
+    # new fields
     picture = models.ImageField()
-    pic_privacy = models.CharField(max_length=1, choices=PRIVACY_CHOICES)
     birthday = models.DateField()
-    birthday_privacy = models.CharField(max_length=1, choices=PRIVACY_CHOICES)
     phone = models.IntegerField(max_length=11)
-    phone_privacy = models.CharField(max_length=1, choices=PRIVACY_CHOICES)
-    name_privacy = models.CharField(max_length=1, choices=PRIVACY_CHOICES)
-    email_privacy = models.CharField(max_length=1, choices=PRIVACY_CHOICES)
 
+
+    # privacy settings
+    pic_privacy = models.CharField(max_length=1, choices=PRIVACY_CHOICES,
+                                   default=('PR', 'Private'))
+    birthday_privacy = models.CharField(max_length=1, choices=PRIVACY_CHOICES,
+                                        default=('PR', 'Private'))
+    phone_privacy = models.CharField(max_length=1, choices=PRIVACY_CHOICES,
+                                     default=('PR', 'Private'))
+    name_privacy = models.CharField(max_length=1, choices=PRIVACY_CHOICES,
+                                    default=('PR', 'Private'))
+    email_privacy = models.CharField(max_length=1, choices=PRIVACY_CHOICES,
+                                     default=('PR', 'Private'))
+    # associates profile to the User model
     user = models.OneToOneField(User)
 
-    @staticmethod
-    def active():
+    @classmethod
+    def active(cls):
         """Returns all active users."""
         return User.objects.filter(is_active=True)
+#   qs = self.get_queryset()
+#   return qs.filter(associated)
 
     def is_active(self):
         return self.user.is_active
@@ -40,3 +53,8 @@ class ImagerProfile(models.Model):
 
     def __unicode__(self):
         return self.user
+
+# create and delete 
+# post_save.connect(create_profile, send=User)
+# pre_delete.connect(delete_user, sender=ImagerProfile)
+
