5e77bb908f4454f8dbeb862f06a87a7592beb8a2
Matthew Lee <ml7@uw.edu>
2015-03-05 04:22:19 -0800

    Modified handler to only listen to signals sent by User objects saving/updating.

--- imager/imagerapp/handlers.py
+++ imager/imagerapp/handlers.py
@@ -1,8 +1,9 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
 
-@receiver(post_save)
+@receiver(post_save, sender=User)
 def imager_signal_handler(sender, **kwargs):
     print sender
     print kwargs
