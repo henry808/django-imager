58a85f5ef7eddd776a10c9a6004f7e4bf138f2bd
Matthew Lee <ml7@uw.edu>
2015-03-06 13:07:41 -0800

    Added migrations.

---
+++ imager/imagerapp/migrations/0004_auto_20150306_0918.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('imagerapp', '0003_imagerprofile_is_active'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='imagerprofile',
+            name='birthday',
+            field=models.DateField(default=datetime.date(2015, 3, 6)),
+            preserve_default=True,
+        ),
+    ]
