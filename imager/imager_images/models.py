397dfb6d88ff831427e976e917e5d75e11ecd50d
Matthew Lee <ml7@uw.edu>
2015-03-07 12:21:17 -0800

    Formed basic model for photos.

--- imager/imager_images/models.py
+++ imager/imager_images/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+class Photo(models.Model):
+    picture = models.ImageField()
+    date_uploaded = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    # TODO: change date_published to only write when an image is shared
+    date_published = models.DateField(auto_now=True)
+
+    title = models.CharField(max_length(50))
+    description = models.CharField(max_length(300), blank=True, null=True)
+
+    published = models.CharField(max_length=2, choices=PRIVACY_CHOICES, default=PRIVATE)
+
+    PRIVATE = 'PR'
+    SHARED = 'SH'
+    PUBLIC = 'PU'
+
+    PRIVACY_CHOICES = (
+        (PRIVATE, 'Private'),
+        (SHARED, 'Shard')
+        (PUBLIC, 'Public'),
+    )
+
+
+class Album(models.Model):
+    pass
