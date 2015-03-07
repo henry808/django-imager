4b4207cb489a742608b8f41930fd8b65bdfda3a6
Matthew Lee <ml7@uw.edu>
2015-03-07 12:35:30 -0800

    Added initial albums model.

--- imager/imager_images/models.py
+++ imager/imager_images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -9,10 +10,8 @@ class Photo(models.Model):
     # TODO: change date_published to only write when an image is shared
     date_published = models.DateField(auto_now=True)
 
-    title = models.CharField(max_length(50))
-    description = models.CharField(max_length(300), blank=True, null=True)
-
-    published = models.CharField(max_length=2, choices=PRIVACY_CHOICES, default=PRIVATE)
+    title = models.CharField(max_length=(50))
+    description = models.CharField(max_length=(300), blank=True, null=True)
 
     PRIVATE = 'PR'
     SHARED = 'SH'
@@ -20,10 +19,33 @@ class Photo(models.Model):
 
     PRIVACY_CHOICES = (
         (PRIVATE, 'Private'),
-        (SHARED, 'Shard')
+        (SHARED, 'Shard'),
         (PUBLIC, 'Public'),
     )
 
+    published = models.CharField(max_length=2, choices=PRIVACY_CHOICES, default=PRIVATE)
+
 
 class Album(models.Model):
-    pass
+    user = models.ForeignKey(User, related_name='albums')
+    photos = models.ManyToManyField(Photo, related_name='albums', null=True, blank=True)
+
+    date_uploaded = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    # TODO: change date_published to only write when an image is shared
+    date_published = models.DateField(auto_now=True)
+
+    title = models.CharField(max_length=(50))
+    description = models.CharField(max_length=(300), blank=True, null=True)
+
+    PRIVATE = 'PR'
+    SHARED = 'SH'
+    PUBLIC = 'PU'
+
+    PRIVACY_CHOICES = (
+        (PRIVATE, 'Private'),
+        (SHARED, 'Shard'),
+        (PUBLIC, 'Public'),
+    )
+
+    published = models.CharField(max_length=2, choices=PRIVACY_CHOICES, default=PRIVATE)
