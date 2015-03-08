abf253d4f8c6cc314a7789f4695537f182420061
Matthew Lee <ml7@uw.edu>
2015-03-07 21:03:22 -0800

    Configured admin template for albums to show dates.

--- imager/imager_images/admin.py
+++ imager/imager_images/admin.py
@@ -8,8 +8,17 @@ class PhotoAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['date_uploaded', 'date_modified', 'date_published']}),
         ('Published', {'fields': ['published']})
     ]
-    readonly_fields = ('date_uploaded', 'date_modified', 'date_published')
+    readonly_fields = ('user', 'date_uploaded', 'date_modified', 'date_published')
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Info', {'fields': ['user', 'title', 'description']}),
+        ('Date information', {'fields': ['date_uploaded', 'date_modified', 'date_published']}),
+        ('Published', {'fields': ['published']})
+    ]
+    readonly_fields = ('user', 'date_uploaded', 'date_modified', 'date_published')
 
 # Register your models here.
 admin.site.register(Photo, PhotoAdmin)
-admin.site.register(Album)
+admin.site.register(Album, AlbumAdmin)
