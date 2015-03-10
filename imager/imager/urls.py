1877fd4a922bfed4b639d423d47c09df5ae88fde
Henry Grantham <hgrantham@gmail.com>
2015-03-09 22:40:18 -0700

    added new url config to point at picture so picture show up on homepage

--- imager/imager/urls.py
+++ imager/imager/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-import imagerapp
+from settings import MEDIA_ROOT
 from registration.backends.simple.views import RegistrationView
 
 urlpatterns = patterns('',
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
     #url(r'^imagerapp/', include(imagerapp.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                 {'document_root': MEDIA_ROOT}),
 )
