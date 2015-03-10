ea764c60d99f3f283322ea787f3ed8fc8f6a5e47
Matthew Lee <ml7@uw.edu>
2015-03-09 17:29:48 -0700

    Added random selection of image object. TODO: get the image to be rendered as an image instead of a filename.

--- imager/imager/views.py
+++ imager/imager/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext, loader
+import random
+
+from imager_images.models import Photo
 
 
 def home(request):
-    context = {'random_image': 'something else', 'request': request}
+    photos = Photo.objects.all()
+    random_image = random.choice(photos)
+
+    context = {'random_image': random_image.picture, 'request': request}
     return render(request, 'home.html', {'context': context})
 
 
