085c296c27e7f8281687cd4553e71e0d1fc35682
Henry Grantham <hgrantham@gmail.com>
2015-03-10 18:34:42 -0700

    added filter to random photos

--- imager/imager/urls.py
+++ imager/imager/urls.py
@@ -1,13 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from settings import MEDIA_ROOT
 from registration.backends.simple.views import RegistrationView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     url(r'^$', 'imager.views.home', name='home'),
     #url(r'^imagerapp/', include(imagerapp.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-                 {'document_root': MEDIA_ROOT}),
-)
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                 {'document_root': settings.MEDIA_ROOT}),
+    # url(r'^static/(?P<path>images/.*)$', 'django.views.static.serve',
+    #              {'document_root': settings.STATIC_ROOT}),
+)   # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+   urlpatterns += patterns('django.contrib.staticfiles.views',
+       url(r'^static/(?P<path>.*)$', 'serve'),
+   )
--- imager/imager/views.py
+++ imager/imager/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import RequestContext, loader
+# from django.http import HttpResponse, HttpResponseRedirect, Http404
+# from django.template import RequestContext, loader
+from settings import MEDIA_URL, STATIC_URL
 import random
 
 from imager_images.models import Photo
 
 
 def home(request):
-    photos = Photo.objects.all()
-    random_image = random.choice(photos)
+    photos = Photo.objects.filter(published=Photo.PRIVATE)
 
-    context = {'random_image': random_image.picture, 'request': request}
+    if photos.count():
+        location = MEDIA_URL
+        random_image = random.choice(photos).picture
+    else:
+        location = STATIC_URL
+        random_image = './images/default_profile_image.jpg'
+    context = {'random_image': random_image,
+               'request': request,
+               'image_location': location}
     return render(request, 'home.html', {'context': context})
