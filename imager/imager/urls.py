47b385f3d3fa94f9b44394f7a4214e635526668d
Henry Grantham <hgrantham@gmail.com>
2015-03-11 13:16:12 -0700

    fixed urls to use serve from static and media if DEBUG is on

--- imager/imager/urls.py
+++ imager/imager/urls.py
@@ -2,20 +2,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 # from registration.backends.simple.views import RegistrationView
 from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     url(r'^$', 'imager.views.home', name='home'),
     #url(r'^imagerapp/', include(imagerapp.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-                 {'document_root': settings.MEDIA_ROOT}),
     # url(r'^static/(?P<path>images/.*)$', 'django.views.static.serve',
     #              {'document_root': settings.STATIC_ROOT}),
-)   # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
 
 if settings.DEBUG:
-   urlpatterns += patterns('django.contrib.staticfiles.views',
-       url(r'^static/(?P<path>.*)$', 'serve'),
-   )
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
