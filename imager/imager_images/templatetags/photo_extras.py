66a6335d76194ac9a60d17493668077f30e5ea05
Matthew Lee <ml7@uw.edu>
2015-03-11 16:37:33 -0700

    Added a custom filter for stream page fir a logged in user.

---
+++ imager/imager_images/templatetags/photo_extras.py
@@ -0,0 +1,17 @@
+from django import template
+from imager_images.models import Photo
+from django.db.models import Q
+
+
+register = template.Library()
+
+SHARED = 'SH'
+PUBLIC = 'PU'
+
+
+# Custom filter, return photos that are public, shared, or belong to a logged in user
+@register.filter
+def viewable(self, user):
+    return Photo.objects.filter(Q(user=user) |
+                                Q(published=SHARED) |
+                                Q(published=PUBLIC))
