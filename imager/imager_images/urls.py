0ded265334e8abfd6b7d736146d580800a10e23c
Matthew Lee <ml7@uw.edu>
2015-03-12 13:21:48 -0700

    Added a generic create view for albums.

--- imager/imager_images/urls.py
+++ imager/imager_images/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
-from imager_images.views import StreamView
+from imager_images.views import StreamView, UploadPhoto
 
 
 urlpatterns = patterns('',
@@ -10,5 +10,13 @@ urlpatterns = patterns('',
                            name='stream'),
                        url(r'library/(?P<pk>\d+)$',
                            'imager_images.views.library_view',
-                           name='library')
+                           name='library'),
+                       url(r'^upload_photo$',
+                           login_required(UploadPhoto.as_view(
+                               template_name='upload_photo.html')),
+                           name='upload_photo'),
                        )
+                       url(r'^create_album$',
+                           login_required(CreateAlbum.as_view(
+                               template_name='create_album.html')),
+                           name='create_album')
--- imager/imager_images/views.py
+++ imager/imager_images/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 from imager_images.models import Photo
 from django.contrib.auth.decorators import login_required
 #from imager_users.forms import ImagerProfileEditForm # to override form
 
 
-# @login_required
 class StreamView(ListView):
     model = Photo
     template_name = "stream.html"
+
+
+class UploadPhoto(CreateView):
+    model = Photo
+    template_name = "upload_photo.html"
+
+
+class CreateAlbum(CreateView):
+    model = Album
+    template_name = "create_album"
 
 
 @login_required
