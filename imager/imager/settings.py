63371bba87369ee08dcef12495f9c3d0b6dbab10
Henry Grantham <hgrantham@gmail.com>
2015-03-21 23:12:27 -0700

    fixed paths to static root and BASE_DIR in settings.py

--- imager/imager/settings.py
+++ imager/imager/settings.py
@@ -13,9 +13,11 @@ import os
 
 from configurations import Configuration
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 class Base(Configuration):
-    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+    BASE_DIR = BASE_DIR
 
     # Quick-start development settings - unsuitable for production
     # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -69,16 +71,16 @@ class Base(Configuration):
     # Database
     # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'imager',
-            'USER': 'JustinKan',
-            'PASSWORD': '',
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
-        }
-    }
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #         'NAME': 'imager',
+    #         'USER': 'JustinKan',
+    #         'PASSWORD': '',
+    #         'HOST': '127.0.0.1',
+    #         'PORT': '5432',
+    #     }
+    # }
 
     # DATABASES = {
     #     'default': {
@@ -172,7 +174,10 @@ class Dev(Base):
 
 
 class Prod(Base):
+
     DEBUG = False
+
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
 
     INSTALLED_APPS = (
         'django.contrib.admin',
