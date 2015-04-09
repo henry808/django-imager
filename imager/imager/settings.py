4a1641a8efc72d28b1c2183d54f37acde9817066
Henry Grantham <hgrantham@gmail.com>
2015-04-09 10:14:21 -0700

    removed some settings to see if it fixes gunicorn errors

--- imager/imager/settings.py
+++ imager/imager/settings.py
@@ -206,9 +206,9 @@ class Prod(Base):
     ALLOWED_HOSTS = ['ec2-54-69-236-218.us-west-2.compute.amazonaws.com',
                      'ec2-54-68-234-113.us-west-2.compute.amazonaws.com', ]
 
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 25
-    EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.environ.get('HOST_USER', '')
-    EMAIL_HOST_PASSWORD = os.environ.get('HOST_PASSWORD', '')
-    DEFAULT_FROM_EMAIL = os.environ.get('HOST_USER', '')
+    # EMAIL_HOST = 'smtp.gmail.com'
+    # EMAIL_PORT = 25
+    # EMAIL_USE_TLS = True
+    # EMAIL_HOST_USER = os.environ.get('HOST_USER', '')
+    # EMAIL_HOST_PASSWORD = os.environ.get('HOST_PASSWORD', '')
+    # DEFAULT_FROM_EMAIL = os.environ.get('HOST_USER', '')
